e11156ccacf55542ec01625786bf03c8af79feef
Saket Shukla <148642140+saket-shukla-0909@users.noreply.github.com>
2024-07-16 00:10:03 +0530

    Home file and css of Redux commit

--- redux/src/components/Home.js
+++ redux/src/components/Home.js
@@ -1,13 +1,21 @@
 import React from "react";
-
+import { FaCartPlus } from "react-icons/fa";
 function Home(){
     return(
-        <div>
-            <h1>Home Component</h1>
+        <div >
+            <div className="home-firstdiv">
+                <div className="text-home">Home Component</div>
+                <div className="add-to-cart">{<FaCartPlus/>}</div>
+            </div>
             <div className="cart-wrapper">
                 <div className="img-wrapper item">
-                    <img />
-
+                    <img src="https://uploads.tapatalk-cdn.com/20180121/30a10cb3f7febc8cf7f6a30a675fb3f9.jpg"/>
+                </div>
+                <div className="text-wrapper item">
+                    Iphone Price: $1000.00
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add to Cart</button>
                 </div>
             </div>
         </div>
