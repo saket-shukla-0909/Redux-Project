3824b0f350e48ead6eeaf7bb23d32807c8106d5f
Saket Shukla <148642140+saket-shukla-0909@users.noreply.github.com>
2024-07-15 23:34:04 +0530

    first commit README.md

---
+++ redux/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div>
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img />
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
